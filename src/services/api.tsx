ce58d3fdb89f12c32d99fb90ceb938b9acbeff2a
BG <mavie1322@yahoo.fr>
2022-05-11 00:38:21 +0100

    store functions that will retrieve information from the database

---
+++ src/services/api.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const marketApi = axios.create({
+  baseURL: "https://nc-marketplace.herokuapp.com/api",
+});
+
+export const getItems = async () => {
+  const res = await marketApi.get("/items");
+  return res.data;
+};
+
+export const getCategories = async () => {
+  const res = await marketApi.get("/categories");
+  return res.data;
+};
