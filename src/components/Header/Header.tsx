6a1e770ab6d616c052708e1060589977a6c204bd
BG <mavie1322@yahoo.fr>
2022-05-10 23:43:34 +0100

    add create a small menu and move the logo when the screen is small

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 import { BsPerson, BsBasket3 } from "react-icons/bs";
+import { RiMenu2Line, RiCloseLine } from "react-icons/ri";
 import { IoSearchOutline } from "react-icons/io5";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { fetchCategories } from "../../store/categories-action";
 
 const Header: React.FC = () => {
+  const categoriesList = useAppSelector((state) => state.categories.categories);
+  const dispatch = useAppDispatch();
+
+  const [toggleMenu, setToggleMenu] = useState<boolean>(false);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
   return (
     <div className='header section__margin'>
       <div className='header__navbar'>
         <div className='header__navbar-container'>
-          <div>toggle box</div>
-          <div>Logo</div>
+          <div className='header__navbar-menu'>
+            {toggleMenu ? (
+              <RiCloseLine
+                className='header__navbar-menu_icons'
+                color='#000'
+                size={27}
+                onClick={() => setToggleMenu(false)}
+              />
+            ) : (
+              <RiMenu2Line
+                className='header__navbar-menu_icons'
+                color='#000'
+                size={27}
+                onClick={() => setToggleMenu(true)}
+              />
+            )}
+            {toggleMenu && (
+              <div className='header__navbar-menu_container scale-up-ver-top'>
+                {categoriesList.map((category) => {
+                  return (
+                    <p key={category.category_name}>{category.category_name}</p>
+                  );
+                })}
+              </div>
+            )}
+          </div>
+          <div className='header__small-logo'>
+            <span>
+              <p>E</p>
+              <p>.com</p>
+            </span>
+          </div>
         </div>
         <div className='header__icons'>
           <div className='header__icons-container'>
@@ -26,7 +68,17 @@ const Header: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className='header__logo'>Logo</div>
+      <div className='header__logo'>
+        <span>
+          <p>E</p>
+          <p>.com</p>
+        </span>
+      </div>
+      <div className='header__categories'>
+        {categoriesList.map((category) => {
+          return <p key={category.category_name}>{category.category_name}</p>;
+        })}
+      </div>
     </div>
   );
 };
