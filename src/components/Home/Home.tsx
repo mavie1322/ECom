1038f848f3379ae8bdb1b382f216abcdf82b15a6
BG <mavie1322@yahoo.fr>
2022-05-11 00:27:59 +0100

    map through the items list

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import { useEffect } from "react";
+import SingleItem from "../../containers/SingleItems/SingleItem";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { Article } from "../../models";
+import { fetchItems } from "../../store/items-actions";
 import "./home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const itemsList = useAppSelector((state) => state.items.itemsList);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchItems());
+  }, [dispatch]);
+
+  return (
+    <div className='home section__margin'>
+      {itemsList.map((item: Article) => {
+        return <SingleItem key={item.item_id} {...item} />;
+      })}
+    </div>
+  );
 };
 
 export default Home;
