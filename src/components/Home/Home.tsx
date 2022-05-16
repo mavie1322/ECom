682f57b4e5e45f51777d1eb046e55859b3d682a8
BG <mavie1322@yahoo.fr>
2022-05-16 10:15:11 +0100

    load the items per category

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import SingleItem from "../../containers/SingleItems/SingleItem";
+import { Link } from "react-router-dom";
+import Items from "../../containers/Items";
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import { Article } from "../../models";
 import { fetchItems } from "../../store/items-actions";
@@ -7,19 +8,24 @@ import "./home.css";
 
 const Home = () => {
   const itemsList = useAppSelector((state) => state.items.itemsList);
+  const pickedCat = useAppSelector(
+    (state) => state.categories.selected_category
+  );
+  console.log(typeof pickedCat);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchItems());
   }, [dispatch]);
 
-  return (
-    <div className='home section__margin'>
-      {itemsList.map((item: Article) => {
-        return <SingleItem key={item.item_id} {...item} />;
-      })}
-    </div>
-  );
+  if (pickedCat) {
+    let pickedCategory: Article[] = itemsList.filter(
+      (item) => item.category_name === pickedCat
+    );
+    return <Items itemsList={pickedCategory} />;
+  }
+
+  return <Items itemsList={itemsList} />;
 };
 
 export default Home;
