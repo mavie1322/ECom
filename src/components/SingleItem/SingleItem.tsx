c76c49214cd21dacf5a5a678821e8d11b21811dd
BG <mavie1322@yahoo.fr>
2022-05-23 14:21:04 +0100

    fetch the item from the api instead of finding the item from the list of items stored. A bug was detected when I was trying to display item from the basket submenu.

--- src/components/SingleItem/SingleItem.tsx
+++ src/components/SingleItem/SingleItem.tsx
@@ -1,20 +1,29 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { useAppDispatch } from "../../hooks/hooks";
 import { Article } from "../../models";
+import { getItemById } from "../../services/api";
 import { basketActions } from "../../store/basket-slices";
 
 import "./singleItem.css";
 
 const SingleItem = () => {
   const { item_id } = useParams<string>();
-  const itemsList = useAppSelector((state) => state.items.itemsList);
-  const basket = useAppSelector((state) => state.basket);
   const dispatch = useAppDispatch();
-  let item: Article | undefined = itemsList.find(
-    (product) => product.item_id === parseInt(item_id!) //even tough it can be null, we assured the compiler it is not
-  );
+  const [singleItemInformation, setSingleItemInformation] = useState<Article>({
+    item_id: 0,
+    item_name: "",
+    price: 0,
+    category_name: "",
+    description: "",
+    img_url: "",
+  });
+  const { item_name, price, description, img_url } = singleItemInformation;
 
-  console.log(basket);
+  const apiItemById = async (id: string) => {
+    const itemFromApi: { item: Article } = await getItemById(id);
+    setSingleItemInformation(itemFromApi.item);
+  };
   const addToBasketHandler = (item: Article) => {
     const itemToAdd = {
       item_basket: item,
@@ -23,19 +32,25 @@ const SingleItem = () => {
     dispatch(basketActions.addItemToBasket(itemToAdd));
   };
 
+  useEffect(() => {
+    apiItemById(item_id!);
+  }, [item_id]);
+
   return (
     <div className='singleItem__container section__margin'>
       <div className='singleItem__container-image'>
-        <img src={item?.img_url} alt={item?.item_name} />
+        <img src={img_url} alt={item_name} />
       </div>
       <div className='singleItem__container-info'>
-        <span>{item?.item_name}</span>
-        <span>£ {item?.price}</span>
+        <span>{item_name}</span>
+        <span>£ {price}</span>
         <hr />
-        <span>{item?.description}</span>
+        <span>{description}</span>
         <hr />
         <span>
-          <button onClick={() => addToBasketHandler(item!)}>Add</button>
+          <button onClick={() => addToBasketHandler(singleItemInformation)}>
+            Add
+          </button>
         </span>
       </div>
     </div>
