cd8711b8916ea245ea0612651bf9100dcae8eaba
BG <mavie1322@yahoo.fr>
2022-05-23 13:50:27 +0100

    show or hide the submenu when the mouse is over the icon or the submenu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./header.css";
 import { BsPerson, BsBasket3 } from "react-icons/bs";
 import { RiMenu2Line, RiCloseLine } from "react-icons/ri";
@@ -8,6 +8,7 @@ import { fetchCategories } from "../../store/categories-action";
 import Categories from "../../containers/Categories";
 import { categoriesActions } from "../../store/categories-slices";
 import { Link } from "react-router-dom";
+import BasketSubmenu from "../Basket/Submenu/BasketSubmenu";
 
 const Header: React.FC = () => {
   const categoriesList = useAppSelector((state) => state.categories.categories);
@@ -16,7 +17,10 @@ const Header: React.FC = () => {
   );
   const dispatch = useAppDispatch();
   const [toggleMenu, setToggleMenu] = useState<boolean>(false);
-  const [isHovering, setIsHovering] = useState<boolean>(false);
+  const [isHoveringBasket, setIsHoveringBasket] = useState<boolean>(false);
+  const [isHoveringSignIn, setIsHoveringSignIn] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+  const signInRef = useRef(null);
 
   const selectCategoryHandler = () => {
     dispatch(categoriesActions.pickedCategory(""));
@@ -31,6 +35,7 @@ const Header: React.FC = () => {
       <div className='header__navbar'>
         <div className='header__navbar-container'>
           <div className='header__navbar-menu'>
+            {/* show categories menu and logo when the screen size is less than 1050px */}
             {toggleMenu ? (
               <RiCloseLine
                 className='header__navbar-menu_icons'
@@ -61,11 +66,24 @@ const Header: React.FC = () => {
             </Link>
           </div>
         </div>
+        {/* header icons sign in search and basket */}
         <div className='header__icons'>
-          <div className='header__icons-container'>
+          <div
+            className='header__icons-container link header__navbar-menu'
+            onMouseOver={() => setIsHoveringSignIn(true)}
+            onMouseOut={() => setIsHoveringSignIn(false)}>
             <BsPerson className='header__icons-size' />
+            {/* if user logged in his name should appear or sign in */}
             <p>Sign In</p>
           </div>
+          {isHoveringSignIn && isLoggedIn && (
+            <div
+              className='header__navbar-menu_container header__sign-hover scale-up-ver-top'
+              onMouseOver={() => setIsHoveringSignIn(true)}
+              onMouseOut={() => setIsHoveringSignIn(false)}>
+              Hello
+            </div>
+          )}
           <div className='header__icons-container'>
             <IoSearchOutline className='header__icons-size' />
             <p>Search</p>
@@ -73,17 +91,19 @@ const Header: React.FC = () => {
           <div>
             <Link
               to={`/users/${"Paul-R"}/basket`}
-              className='link header__icons-container header__navbar-menu'>
+              className='link header__icons-container header__navbar-menu'
+              onMouseOver={() => setIsHoveringBasket(true)}
+              onMouseOut={() => setIsHoveringBasket(false)}>
               <BsBasket3 className='header__icons-size' />
-              <p
-                onMouseEnter={() => setIsHovering(true)}
-                onMouseLeave={() => setIsHovering(false)}>
-                Basket({basketTotalQuantity})
-              </p>
+              <p>Basket({basketTotalQuantity})</p>
             </Link>
-            {isHovering && (
-              <div className='header__navbar-menu_container header__navbar-hover'>
-                Hello
+            {/* show the submenu basket when user hover on the basket icon when the screen width is over 1050px */}
+            {isHoveringBasket && (
+              <div
+                className='header__navbar-menu_container header__basket-hover scale-up-ver-top'
+                onMouseOver={() => setIsHoveringBasket(true)}
+                onMouseOut={() => setIsHoveringBasket(false)}>
+                <BasketSubmenu />
               </div>
             )}
           </div>
