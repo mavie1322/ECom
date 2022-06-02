6990602ef6a1d8644e8ccf22ee7315018c05e766
BG <mavie1322@yahoo.fr>
2022-06-02 23:17:10 +0100

    add user reducer

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { basketSlice } from "./basket-slices";
 import { categoriesSlice } from "./categories-slices";
 import { itemsSlice } from "./items-slice";
+import { userSlice } from "./user-slices";
 
 const store = configureStore({
   reducer: {
     items: itemsSlice.reducer,
     categories: categoriesSlice.reducer,
     basket: basketSlice.reducer,
+    user: userSlice.reducer,
   },
 });
 
